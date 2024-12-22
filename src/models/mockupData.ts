c84bdaea3699077b08f1c6ffb6a825067ba6eefe
Eric Njanga <eric.njanga@gmail.com>
2024-12-22 10:56:17 -0500

    Updating mockup data

--- src/models/mockupData.ts
+++ src/models/mockupData.ts
@@ -23,23 +23,26 @@ export const mockInformationCard1DataArray = [
     id: '2', 
     parentId: '2',
     blurb: `Translating visual designs into responsive and interactive User Interfaces.`,
-    description: `<h3> (Expertise 1)</h3>
-      <p><b>Description 2 ...</b></p>
+    title: 'Expertise 1',
+    description: ` 
+      <p><b>Description 1 ...</b></p>
       <p>Macaroon croissant sesame snaps liquorice jelly-o toffee. Brownie marshmallow shortbread cake pudding liquorice dragée cupcake cake. Dragée jelly beans cake carrot cake dragée dessert apple pie. Dragée macaroon pie sugar plum wafer.</p>
     `, 
   },{
     id: '3', 
     parentId: '2',
     blurb: `Creating Responsive and interactive User Interfaces.`,
-    description: `<h3> (Expertise 1)</h3>
-      <p><b>Description 3 ...</b></p>
+    title: 'Expertise 2',
+    description: `
+      <p><b>Description 2 ...</b></p>
       <p>Macaroon croissant sesame snaps liquorice jelly-o toffee. Brownie marshmallow shortbread cake pudding liquorice dragée cupcake cake. Dragée jelly beans cake carrot cake dragée dessert apple pie. Dragée macaroon pie sugar plum wafer.</p>
     `, 
   },{
     id: '4', 
     parentId: '2',
     blurb: `Ensuring Accessibility compliance ...`,
-    description: `<h3> (Expertise 1)</h3>
+    title: 'Expertise 3',
+    description: ` 
       <p><b>Description 4 ...</b></p>
       <p>Macaroon croissant sesame snaps liquorice jelly-o toffee. Brownie marshmallow shortbread cake pudding liquorice dragée cupcake cake. Dragée jelly beans cake carrot cake dragée dessert apple pie. Dragée macaroon pie sugar plum wafer.</p>
     `,  
@@ -47,7 +50,8 @@ export const mockInformationCard1DataArray = [
     id: '5', 
     parentId: '2',
     blurb: `Solving complex and unusual coding problems and communication effectively with stakeholders`,
-    description: `<h3> (Expertise 1)</h3>
+    title: 'Expertise 4',
+    description: ` 
       <p><b>Description 5 ...</b></p>
       <p>Macaroon croissant sesame snaps liquorice jelly-o toffee. Brownie marshmallow shortbread cake pudding liquorice dragée cupcake cake. Dragée jelly beans cake carrot cake dragée dessert apple pie. Dragée macaroon pie sugar plum wafer.</p>
     `, 
@@ -55,7 +59,8 @@ export const mockInformationCard1DataArray = [
     id: '6', 
     parentId: '6',
     blurb: `Expertise specification 2 - blurb 1`,
-    description: `<h3> (Expertise 2)</h3>
+    title: 'Expertise 5',
+    description: `
       <p><b>Description 1 ...</b></p>
       <p>Macaroon croissant sesame snaps liquorice jelly-o toffee. Brownie marshmallow shortbread cake pudding liquorice dragée cupcake cake. Dragée jelly beans cake carrot cake dragée dessert apple pie. Dragée macaroon pie sugar plum wafer.</p>
     `, 
@@ -64,20 +69,19 @@ export const mockInformationCard1DataArray = [
  
 
 export const mockModalContext = {
-  isOpen: false,              
-  renderingType: 'data', 
+  isOpen: false,             
+  childComponent: '',
   modalData : {
     ...mockInformationCard1DataArray[0]
   },
   openModal: ({ 
-    renderingType, 
     dataType, 
-    dataId }: { 
-      renderingType: string, 
+    dataId 
+  } : {  
       dataType: string, 
       dataId: string 
   }) => {
-    console.log(`Modal opened with renderingType ${renderingType} | dataType ${dataType} | dataId ${dataId} | .`);
+    console.log(`Modal opened with dataType ${dataType} | dataId ${dataId} | .`);
   },
   closeModal: () => {
     console.log(`Modal closed`);
