aef596b4dfaf9198fe545f0e0b910d4f2bb5ad5b
Eric Njanga <eric.njanga@gmail.com>
2025-05-10 10:19:01 -0400

    Adapting JSX markup to contentful data

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,52 +1,27 @@
-import "./Hero.scss"; 
+import "./Hero.scss";
 import React, { useContext } from "react";
 import Heading from "../Heading/Heading";
 import Button from "../Button/Button";
 import { useTranslation } from "react-i18next";
 import { LanguageContext } from "../../utils/contexts";
 import Preloader from "../Preloader/Preloader";
-  
+
 import { useContentful } from "../../hooks/useContentful";
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { queryData, queryKeyData, sectionId } from "./Hero.shared";
-
-
-// export function PagesssSections({ sectionId = '2IESwjrb2JUTQG0S8PZ65S', locale = 'fr'}) {
- 
-//   const { data, isLoading, error } = useContentful({
-//     query: queryData,
-//     variables: { sectionId, locale },
-//     queryKey: queryKeyData
-//   });
-
-//   if (isLoading) return <p>Loading...</p>;
-//   if (error) return <p>Error loading blog posts.</p>;
-
-//   console.log('********** title = ', data.pageSection.title);
-//   console.log('********** description = ', documentToReactComponents(data.pageSection.description.json));
- 
-//   return (
-//     <div>
-//       <h3>{data.pageSection.title}</h3>
-//       <div>{documentToReactComponents(data.pageSection.description.json)}</div>
-//     </div>
-//   );
-
-// }
-
 
 
 const Hero = () => {
   // For extracting localised content from "i18n.ts" file based on the currently active locale
   const { t } = useTranslation();
-  
+
   // Getting the currently active locale...
   const activeLang = useContext(LanguageContext);
 
-  /** 
-   * Fetching ContentFul data in all languages, and handling errors and loading time 
+  /**
+   * Fetching ContentFul data in all languages, and handling errors and loading time
    * ----------------------
-  */
+   */
   const { data, isLoading, error } = useContentful({
     query: queryData,
     variables: { sectionId, locale1: "en-CA", locale2: "fr" },
@@ -59,11 +34,10 @@ const Hero = () => {
   }
   // Display an error messaye if there was problem fetching data
   if (error) return <p>{t("ErrorLoadingPosts")}</p>;
-  /** 
-   * Fetching ContentFul data in all languages, and handling errors and loading time 
+  /**
+   * Fetching ContentFul data in all languages, and handling errors and loading time
    * ----------------------
-  */
-  
+   */
 
   return (
     <header className="Hero">
@@ -75,14 +49,15 @@ const Hero = () => {
                 {data[activeLang]?.title}
               </Heading>
               <div
-  className="Hero-subtitle"
-  dangerouslySetInnerHTML={{
-    __html: String(data[activeLang]?.description?.json ?? ''),
-  }}
-/>
-              {/* <div className="Hero-subtitle">
-                {data[activeLang]?.description?.json && documentToReactComponents(data[activeLang]?.description?.json)}
-              </div> */}
+                className="Hero-subtitle"
+                dangerouslySetInnerHTML={{
+                  __html: String(
+                    data[activeLang]?.description?.json?.content[0]?.content[0]
+                      ?.value ?? ""
+                  ),
+                }}
+              />
+              {/* <div>{documentToReactComponents(data.pageSection.description.json)}</div> */}
             </>
           )}
         </div>
