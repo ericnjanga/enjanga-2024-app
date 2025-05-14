72282f315b60671d901fd3d136406e6c4949ac55
Eric Njanga <eric.njanga@gmail.com>
2025-05-14 14:23:51 -0400

    Hiding temporary elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router /*, Route, Routes*/} from "react-router-dom";
 import "./styles/App.scss";
 import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
-import CarrouselExpertise from "./components/Expertise/Expertise";
+// import CarrouselExpertise from "./components/Expertise/Expertise";
 import CarrouselPortfolio from "./components/Portfolio/Portfolio";
 import About from "./components/About/About";
 import Modal from "./components/ModalModule";
@@ -12,8 +12,6 @@ import { ModalProvider } from "./components/ModalModule";
 import Navigation from "./components/Navigation/Navigation";
 import { LanguageProvider } from "./components/LanguageModule";
 
-import { useEffect, useState } from "react";
-import axios from "axios";
 
 // Ensuring that Bootstrap's JavaScript is globally available across all components.
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -21,8 +19,6 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // We'll use slick-carousel in two compoment, so we might as well import it globally
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-const Home = () => <p>*** Routes setup ***</p>;
 
 const queryClient = new QueryClient();
 
@@ -54,10 +50,8 @@ function App() {
               <Footer />
 
               {/* <Routes>
-                <Route path="/" element={<Home />} />
-                
-                  <Route id="about" path="/about" element={<About />} />
-              
+                <Route path="/" element={<Home />} /> 
+                  <Route id="about" path="/about" element={<About />} /> 
               </Routes> */}
             </div>
           </Router>
