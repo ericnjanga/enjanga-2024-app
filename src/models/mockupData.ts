4bf7c3ea5a163787c259754c1d46360186cfde0c
Eric Njanga <eric.njanga@gmail.com>
2025-05-30 08:04:24 -0400

    Cleaning up unecessary mocked data

--- src/models/mockupData.ts
+++ src/models/mockupData.ts
@@ -1,233 +1,4 @@
 
-export const mockNavOptions = [
-  {
-    id: '1',
-    name: 'Nav item 1', 
-    url: '#nav-item-1',
-    description: '....', 
-  }, {
-    id: '2',
-    name: 'Nav item 2', 
-    url: '#nav-item-2',
-    description: '....', 
-  }, {
-    id: '3',
-    name: 'Nav item 3', 
-    url: '#nav-item-3',
-    description: '....', 
-  }
-];
-
-export const mockInformationCard1DataArray = [
-  {
-    id: '2', 
-    parentId: '2',
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  },{
-    id: '3', 
-    parentId: '2',
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  },{
-    id: '4', 
-    parentId: '2',
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  },{
-    id: '5', 
-    parentId: '2',
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  },{
-    id: '6', 
-    parentId: '6',
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  }
-];
-
-export const mockProjectThumbDataArray = [
-  {
-    id: '1',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    image: 'images/project-1.png',
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-  },{
-    id: '2',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    image: 'images/project-2.png',
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-  },{
-    id: '3',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    image: 'images/project-3.png',
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-  },{
-    id: '4',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    blurb: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-    image: 'images/project-4.png',
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },
-  }
-];
- 
-
-export const mockModalContext = {
-  isOpen: false,             
-  childComponent: '',
-  modalData : {
-    ...mockInformationCard1DataArray[0]
-  },
-  openModal: ({ 
-    dataType, 
-    dataId 
-  } : {  
-      dataType: string, 
-      dataId: string 
-  }) => {
-    console.log(`Modal opened with dataType ${dataType} | dataId ${dataId} | .`);
-  },
-  closeModal: () => {
-    console.log(`Modal closed`);
-  },
-  submitModalForm: () => {}
-};
-
-
-export const mockPageSectionsProps = [
-  {
-    id: '1',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },  
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  }, { // Expertise specification 1
-    id: '2',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },  
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },  
-  }, { // Portfolio
-    id: '3',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },  
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  }, { // Expertise specification 1
-    id: '4',
-    title: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    },  
-    description: {
-      en: `Translating visual designs into responsive and interactive User Interfaces.`,
-      fr: `Translating visual designs into responsive and interactive User Interfaces.`
-    }, 
-  }
-];
-
 export const mockContactForm = {
   initValues: {
     name: "",
@@ -282,5 +53,236 @@ export const mockContactForm = {
   },
 
 };
+
+
+// export const mockNavOptions = [
+//   {
+//     id: '1',
+//     name: 'Nav item 1', 
+//     url: '#nav-item-1',
+//     description: '....', 
+//   }, {
+//     id: '2',
+//     name: 'Nav item 2', 
+//     url: '#nav-item-2',
+//     description: '....', 
+//   }, {
+//     id: '3',
+//     name: 'Nav item 3', 
+//     url: '#nav-item-3',
+//     description: '....', 
+//   }
+// ];
+
+// export const mockInformationCard1DataArray = [
+//   {
+//     id: '2', 
+//     parentId: '2',
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   },{
+//     id: '3', 
+//     parentId: '2',
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   },{
+//     id: '4', 
+//     parentId: '2',
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   },{
+//     id: '5', 
+//     parentId: '2',
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   },{
+//     id: '6', 
+//     parentId: '6',
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   }
+// ];
+
+// export const mockProjectThumbDataArray = [
+//   {
+//     id: '1',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     image: 'images/project-1.png',
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//   },{
+//     id: '2',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     image: 'images/project-2.png',
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//   },{
+//     id: '3',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     image: 'images/project-3.png',
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//   },{
+//     id: '4',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     blurb: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//     image: 'images/project-4.png',
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },
+//   }
+// ];
+ 
+
+// export const mockModalContext = {
+//   isOpen: false,             
+//   childComponent: '',
+//   modalData : {
+//     ...mockInformationCard1DataArray[0]
+//   },
+//   openModal: ({ 
+//     dataType, 
+//     dataId 
+//   } : {  
+//       dataType: string, 
+//       dataId: string 
+//   }) => {
+//     console.log(`Modal opened with dataType ${dataType} | dataId ${dataId} | .`);
+//   },
+//   closeModal: () => {
+//     console.log(`Modal closed`);
+//   },
+//   submitModalForm: () => {}
+// };
+
+
+// export const mockPageSectionsProps = [
+//   {
+//     id: '1',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },  
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   }, { // Expertise specification 1
+//     id: '2',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },  
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },  
+//   }, { // Portfolio
+//     id: '3',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },  
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   }, { // Expertise specification 1
+//     id: '4',
+//     title: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     },  
+//     description: {
+//       en: `Translating visual designs into responsive and interactive User Interfaces.`,
+//       fr: `Translating visual designs into responsive and interactive User Interfaces.`
+//     }, 
+//   }
+// ];
+
 
   
